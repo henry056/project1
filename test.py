03d2ab26038ff9f00cc937325f793d512d96d2c3
henry056 <151273165+henry056@users.noreply.github.com>
2023-11-18 10:25:49 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import pandas as pd
-data=pd.read_csv('경찰청_범죄 발생 지역별 통계_20151231.csv', sep='delimiter' , error_bad_lines=False)
+data=pd.read_csv('경찰청_범죄 발생 지역별 통계_20151231.csv', sep='delimiter')
 st.title("지역별 범죄에 대한 조사")
